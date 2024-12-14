c2e70c51eaa5e123e7175633d8966137f90fcee3
David Agredano <davidagredano@gmail.com>
2024-12-14 20:37:36 +0100

    Add ex13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/14 20:17:43 by dagredan          #+#    #+#             */
+/*   Updated: 2024/12/14 20:22:55 by dagredan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0 || nb > 12)
+		return (0);
+	else if (nb == 1 || nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
